a7c873821972ae9cb61ecbbc214a7bad3795072a
Aida <74357150+aida22@users.noreply.github.com>
2020-12-25 17:40:39 +0300

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,17 @@
+var randomNumber = (Math.floor(Math.random() * 6) + 1);
+var randomImg = "image/dice" + randomNumber + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImg);
+
+var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
+var randomImg2 = "image/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+
+if (randomNumber > randomNumber2){
+  document.querySelector('h1').innerHTML = "🚩Player 1 Wins"
+}
+else if (randomNumber2 >randomNumber) {
+  document.querySelector('h1').innerHTML = "Player 2 Wins🚩"
+}
+else {
+  document.querySelector('h1').innerHTML = "Draw!"
+}
